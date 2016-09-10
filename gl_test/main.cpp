46306b02e65e16d3a3c5751944132cfaacc1360c
maveric_94 <maveric94@mail.ru>
2016-09-10 16:54:27 +0300

    Added project

---
+++ gl_test/main.cpp
@@ -0,0 +1,37 @@
+#include <SFML\Window.hpp>
+#include <SFML\OpenGL.hpp>
+
+int main()
+{
+	sf::ContextSettings settings;
+	settings.depthBits = 24;
+	settings.stencilBits = 8;
+	settings.antialiasingLevel = 4;
+	settings.majorVersion = 3;
+	settings.minorVersion = 3;
+
+	sf::Window window(sf::VideoMode(800, 600), "OpenGL", sf::Style::Default, settings);
+
+	bool running = true;
+	while (running)
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+			{
+				running = false;
+			}
+			else if (event.type == sf::Event::Resized)
+			{
+				glViewport(0, 0, event.size.width, event.size.height);
+			}
+		}
+
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		window.display();
+	}
+
+	return 0;
+}
